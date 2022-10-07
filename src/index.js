50b39d6eaacea1a3a5617228101423ec51e75ad7
Jon Ursenbach <jon@ursenba.ch>
2022-10-07 13:35:43 -0700

    feat: heavily pairing down the library

--- src/index.js
+++ src/index.js
@@ -5,34 +5,14 @@ const SPEC_ALGORITHMS = ['sha256', 'sha384', 'sha512'];
 // TODO: this should really be a hardcoded list of algorithms we support,
 // rather than [a-z0-9].
 const BASE64_REGEX = /^[a-z0-9+/]+(?:=?=?)$/i;
-const SRI_REGEX = /^([a-z0-9]+)-([^?]+)([?\S*]*)$/;
 // eslint-disable-next-line unicorn/no-unsafe-regex
 const STRICT_SRI_REGEX = /^([a-z0-9]+)-([A-Za-z0-9+/=]{44,88})(\?[\x21-\x7E]*)?$/;
 const VCHAR_REGEX = /^[\x21-\x7E]+$/;
 
-const defaultOpts = {
-  algorithms: ['sha512'],
-  error: false,
-  options: [],
-  pickAlgorithm: getPrioritizedHash,
-  sep: ' ',
-  single: false,
-  strict: false,
-};
-
-const ssriOpts = (opts = {}) => ({ ...defaultOpts, ...opts });
-
 const getOptString = options => (!options || !options.length ? '' : `?${options.join('?')}`);
 
 class Hash {
-  get isHash() {
-    return true;
-  }
-
-  constructor(hash, opts) {
-    // eslint-disable-next-line no-param-reassign
-    opts = ssriOpts(opts);
-    const strict = !!opts.strict;
+  constructor(hash) {
     this.source = hash.trim();
 
     // set default values so that we make V8 happy to
@@ -43,13 +23,13 @@ class Hash {
 
     // 3.1. Integrity metadata (called "Hash" by ssri)
     // https://w3c.github.io/webappsec-subresource-integrity/#integrity-metadata-description
-    const match = this.source.match(strict ? STRICT_SRI_REGEX : SRI_REGEX);
+    const match = this.source.match(STRICT_SRI_REGEX);
     if (!match) {
       return;
-    }
-    if (strict && !SPEC_ALGORITHMS.some(a => a === match[1])) {
+    } else if (!SPEC_ALGORITHMS.some(a => a === match[1])) {
       return;
     }
+
     this.algorithm = match[1];
     this.digest = match[2];
 
@@ -59,323 +39,85 @@ class Hash {
     }
   }
 
-  hexDigest() {
-    return this.digest && Buffer.from(this.digest, 'base64').toString('hex');
-  }
-
   toJSON() {
     return this.toString();
   }
 
-  toString(opts) {
-    // eslint-disable-next-line no-param-reassign
-    opts = ssriOpts(opts);
-    if (opts.strict) {
-      // Strict mode enforces the standard as close to the foot of the
-      // letter as it can.
-      if (
-        !(
-          // The spec has very restricted productions for algorithms.
-          // https://www.w3.org/TR/CSP2/#source-list-syntax
-          (
-            SPEC_ALGORITHMS.some(x => x === this.algorithm) &&
-            // Usually, if someone insists on using a "different" base64, we
-            // leave it as-is, since there's multiple standards, and the
-            // specified is not a URL-safe variant.
-            // https://www.w3.org/TR/CSP2/#base64_value
-            this.digest.match(BASE64_REGEX) &&
-            // Option syntax is strictly visual chars.
-            // https://w3c.github.io/webappsec-subresource-integrity/#grammardef-option-expression
-            // https://tools.ietf.org/html/rfc5234#appendix-B.1
-            this.options.every(opt => opt.match(VCHAR_REGEX))
-          )
+  toString() {
+    if (
+      !(
+        // The spec has very restricted productions for algorithms.
+        // https://www.w3.org/TR/CSP2/#source-list-syntax
+        (
+          SPEC_ALGORITHMS.some(x => x === this.algorithm) &&
+          // Usually, if someone insists on using a "different" base64, we
+          // leave it as-is, since there's multiple standards, and the
+          // specified is not a URL-safe variant.
+          // https://www.w3.org/TR/CSP2/#base64_value
+          this.digest.match(BASE64_REGEX) &&
+          // Option syntax is strictly visual chars.
+          // https://w3c.github.io/webappsec-subresource-integrity/#grammardef-option-expression
+          // https://tools.ietf.org/html/rfc5234#appendix-B.1
+          this.options.every(opt => opt.match(VCHAR_REGEX))
         )
-      ) {
-        return '';
-      }
+      )
+    ) {
+      return '';
     }
+
     const options = this.options && this.options.length ? `?${this.options.join('?')}` : '';
     return `${this.algorithm}-${this.digest}${options}`;
   }
 }
 
-class Integrity {
-  get isIntegrity() {
-    return true;
-  }
-
-  toJSON() {
-    return this.toString();
-  }
-
-  isEmpty() {
-    return Object.keys(this).length === 0;
-  }
-
-  toString(opts) {
-    // eslint-disable-next-line no-param-reassign
-    opts = ssriOpts(opts);
-    let sep = opts.sep || ' ';
-    if (opts.strict) {
-      // Entries must be separated by whitespace, according to spec.
-      sep = sep.replace(/\S+/g, ' ');
-    }
-    return Object.keys(this)
-      .map(k => {
-        if (typeof this[k] === 'function') return false;
-
-        return this[k]
-          .map(hash => {
-            return Hash.prototype.toString.call(hash, opts);
-          })
-          .filter(x => x.length)
-          .join(sep);
-      })
-      .filter(x => x.length)
-      .join(sep);
-  }
-
-  concat(integrity, opts) {
-    // eslint-disable-next-line no-param-reassign
-    opts = ssriOpts(opts);
-    const other = typeof integrity === 'string' ? integrity : stringify(integrity, opts);
-    return parse(`${this.toString(opts)} ${other}`, opts);
-  }
-
-  hexDigest() {
-    return parse(this, { single: true }).hexDigest();
-  }
-
-  // add additional hashes to an integrity value, but prevent
-  // *changing* an existing integrity hash.
-  merge(integrity, opts) {
-    // eslint-disable-next-line no-param-reassign
-    opts = ssriOpts(opts);
-    const other = parse(integrity, opts);
-
-    Object.keys(other).forEach(algo => {
-      if (this[algo]) {
-        if (!this[algo].find(hash => other[algo].find(otherhash => hash.digest === otherhash.digest))) {
-          throw new Error('hashes do not match, cannot update integrity');
-        }
-      } else {
-        this[algo] = other[algo];
-      }
-    });
-  }
-
-  match(integrity, opts) {
-    // eslint-disable-next-line no-param-reassign
-    opts = ssriOpts(opts);
-    const other = parse(integrity, opts);
-    if (!other) {
-      return false;
-    }
-    const algo = other.pickAlgorithm(opts);
-    return (
-      (this[algo] &&
-        other[algo] &&
-        this[algo].find(hash => other[algo].find(otherhash => hash.digest === otherhash.digest))) ||
-      false
-    );
-  }
-
-  pickAlgorithm(opts) {
-    // eslint-disable-next-line no-param-reassign
-    opts = ssriOpts(opts);
-    const pickAlgorithm = opts.pickAlgorithm;
-    const keys = Object.keys(this);
-    return keys.reduce((acc, algo) => {
-      return pickAlgorithm(acc, algo) || acc;
-    });
-  }
-}
-
 module.exports.parse = parse;
-function parse(sri, opts) {
+function parse(sri) {
   if (!sri) {
     return null;
   }
 
-  // eslint-disable-next-line no-param-reassign
-  opts = ssriOpts(opts);
-  if (typeof sri === 'string') {
-    return _parse(sri, opts);
-  } else if (sri.algorithm && sri.digest) {
-    const fullSri = new Integrity();
-    fullSri[sri.algorithm] = [sri];
-    return _parse(stringify(fullSri, opts), opts);
-  }
-
-  return _parse(stringify(sri, opts), opts);
+  return new Hash(sri);
 }
 
-function _parse(integrity, opts) {
-  // 3.4.3. Parse metadata
-  // https://w3c.github.io/webappsec-subresource-integrity/#parse-metadata
-  if (opts.single) {
-    return new Hash(integrity, opts);
-  }
-  const hashes = integrity
-    .trim()
-    .split(/\s+/)
-    .reduce((acc, string) => {
-      const hash = new Hash(string, opts);
-      if (hash.algorithm && hash.digest) {
-        const algo = hash.algorithm;
-        if (!acc[algo]) {
-          acc[algo] = [];
-        }
-        acc[algo].push(hash);
-      }
-      return acc;
-    }, new Integrity());
-  return hashes.isEmpty() ? null : hashes;
-}
-
-module.exports.stringify = stringify;
-function stringify(obj, opts) {
+module.exports.create = create;
+function create(data, opts) {
   // eslint-disable-next-line no-param-reassign
-  opts = ssriOpts(opts);
-  if (obj.algorithm && obj.digest) {
-    return Hash.prototype.toString.call(obj, opts);
-  } else if (typeof obj === 'string') {
-    return stringify(parse(obj, opts), opts);
-  }
+  opts = {
+    algorithm: 'sha512',
+    options: [],
+    ...opts,
+  };
 
-  return Integrity.prototype.toString.call(obj, opts);
-}
-
-module.exports.fromHex = fromHex;
-function fromHex(hexDigest, algorithm, opts) {
-  // eslint-disable-next-line no-param-reassign
-  opts = ssriOpts(opts);
+  const algorithm = opts.algorithm;
   const optString = getOptString(opts.options);
-  return parse(`${algorithm}-${Buffer.from(hexDigest, 'hex').toString('base64')}${optString}`, opts);
+
+  const digest = crypto.createHash(algorithm).update(data).digest('base64');
+  return new Hash(`${algorithm}-${digest}${optString}`, opts);
 }
 
-module.exports.fromData = fromData;
-function fromData(data, opts) {
-  // eslint-disable-next-line no-param-reassign
-  opts = ssriOpts(opts);
-  const algorithms = opts.algorithms;
-  const optString = getOptString(opts.options);
-  return algorithms.reduce((acc, algo) => {
-    const digest = crypto.createHash(algo).update(data).digest('base64');
-    const hash = new Hash(`${algo}-${digest}${optString}`, opts);
-    /* istanbul ignore else - it would be VERY strange if the string we
-     * just calculated with an algo did not have an algo or digest.
-     */
-    if (hash.algorithm && hash.digest) {
-      const hashAlgo = hash.algorithm;
-      if (!acc[hashAlgo]) {
-        acc[hashAlgo] = [];
-      }
-      acc[hashAlgo].push(hash);
+module.exports.verify = verify;
+function verify(data, sri) {
+  try {
+    if (typeof sri === 'object' && sri instanceof Hash) {
+      // eslint-disable-next-line no-param-reassign
+      sri = sri.toString();
     }
-    return acc;
-  }, new Integrity());
-}
 
-module.exports.checkData = checkData;
-function checkData(data, sri, opts) {
-  // eslint-disable-next-line no-param-reassign
-  opts = ssriOpts(opts);
-  // eslint-disable-next-line no-param-reassign
-  sri = parse(sri, opts);
-  if (!sri || !Object.keys(sri).length) {
-    if (opts.error) {
-      throw Object.assign(new Error('No valid integrity hashes to check against'), {
-        code: 'EINTEGRITY',
-      });
-    } else {
+    // eslint-disable-next-line no-param-reassign
+    sri = parse(sri);
+    if (!sri) {
       return false;
     }
+
+    const algorithm = sri.algorithm;
+    const digest = crypto.createHash(algorithm).update(data).digest('base64');
+    const newSri = parse(`${algorithm}-${digest}`);
+
+    return sri.toString() === newSri.toString();
+  } catch (err) {
+    // `crypto.createHash()` will throw errors if `algorithm` is invalid which will happen if we're
+    // supplied with an invalid or corrupt hash. Since we just want this method to only verify if
+    // two given hashes match, we don't want to throw if that happens.
+    return false;
   }
-  const algorithm = sri.pickAlgorithm(opts);
-  const digest = crypto.createHash(algorithm).update(data).digest('base64');
-  const newSri = parse({ algorithm, digest });
-  const match = newSri.match(sri, opts);
-  if (match || !opts.error) {
-    return match;
-  } else if (typeof opts.size === 'number' && data.length !== opts.size) {
-    const err = new Error(`data size mismatch when checking ${sri}.\n  Wanted: ${opts.size}\n  Found: ${data.length}`);
-    err.code = 'EBADSIZE';
-    err.found = data.length;
-    err.expected = opts.size;
-    err.sri = sri;
-    throw err;
-  } else {
-    const err = new Error(
-      `Integrity checksum failed when using ${algorithm}: Wanted ${sri}, but got ${newSri}. (${data.length} bytes)`
-    );
-    err.code = 'EINTEGRITY';
-    err.found = newSri;
-    err.expected = sri;
-    err.algorithm = algorithm;
-    err.sri = sri;
-    throw err;
-  }
-}
-
-module.exports.create = createIntegrity;
-function createIntegrity(opts) {
-  // eslint-disable-next-line no-param-reassign
-  opts = ssriOpts(opts);
-  const algorithms = opts.algorithms;
-  const optString = getOptString(opts.options);
-
-  const hashes = algorithms.map(crypto.createHash);
-
-  return {
-    update(chunk, enc) {
-      hashes.forEach(h => h.update(chunk, enc));
-      return this;
-    },
-    digest(enc) {
-      const integrity = algorithms.reduce((acc, algo) => {
-        const digest = hashes.shift().digest('base64');
-        const hash = new Hash(`${algo}-${digest}${optString}`, opts);
-        /* istanbul ignore else - it would be VERY strange if the hash we
-         * just calculated with an algo did not have an algo or digest.
-         */
-        if (hash.algorithm && hash.digest) {
-          const hashAlgo = hash.algorithm;
-          if (!acc[hashAlgo]) {
-            acc[hashAlgo] = [];
-          }
-          acc[hashAlgo].push(hash);
-        }
-        return acc;
-      }, new Integrity());
-
-      return integrity;
-    },
-  };
-}
-
-const NODE_HASHES = new Set(crypto.getHashes());
-
-// This is a Best Effort™ at a reasonable priority for hash algos
-const DEFAULT_PRIORITY = [
-  'md5',
-  'whirlpool',
-  'sha1',
-  'sha224',
-  'sha256',
-  'sha384',
-  'sha512',
-  // TODO - it's unclear _which_ of these Node will actually use as its name
-  //        for the algorithm, so we guesswork it based on the OpenSSL names.
-  'sha3',
-  'sha3-256',
-  'sha3-384',
-  'sha3-512',
-  'sha3_256',
-  'sha3_384',
-  'sha3_512',
-].filter(algo => NODE_HASHES.has(algo));
-
-function getPrioritizedHash(algo1, algo2) {
-  return DEFAULT_PRIORITY.indexOf(algo1.toLowerCase()) >= DEFAULT_PRIORITY.indexOf(algo2.toLowerCase()) ? algo1 : algo2;
 }
---
+++ test/index.test.js
@@ -0,0 +1,152 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+const { expect } = require('chai');
+
+const ssri = require('../src');
+
+const TEST_DATA = fs.readFileSync(__filename);
+
+function hash(data, algorithm) {
+  return crypto.createHash(algorithm).update(data).digest('base64');
+}
+
+describe('ssri', function () {
+  describe('#create', function () {
+    it('should generate a sha512 hash object from Buffer data', function () {
+      expect(ssri.create(TEST_DATA).toString()).to.equal(`sha512-${hash(TEST_DATA, 'sha512')}`);
+    });
+
+    it('should generate a sha512 hash object from String data', function () {
+      expect(ssri.create(TEST_DATA.toString('utf8')).toString()).to.equal(`sha512-${hash(TEST_DATA, 'sha512')}`);
+    });
+
+    it('should generate a sha256 hash object with `opts.algorithm`', function () {
+      expect(ssri.create(TEST_DATA, { algorithm: 'sha256' }).toString()).to.equal(
+        `sha256-${hash(TEST_DATA, 'sha256')}`
+      );
+    });
+
+    it('should be able to add options to a hash with `opts.options', function () {
+      expect(
+        ssri
+          .create(TEST_DATA, {
+            algorithm: 'sha256',
+            options: ['foo', 'bar'],
+          })
+          .toString()
+      ).to.equal(`sha256-${hash(TEST_DATA, 'sha256')}?foo?bar`);
+    });
+  });
+
+  describe('#parse', function () {
+    it('should parse an integrity string', function () {
+      const sha = hash(TEST_DATA, 'sha512');
+      const integrity = `sha512-${sha}`;
+      expect(ssri.parse(integrity)).to.deep.equal({
+        source: integrity,
+        digest: sha,
+        algorithm: 'sha512',
+        options: [],
+      });
+    });
+
+    it('should parse options from integrity string', function () {
+      const sha = hash(TEST_DATA, 'sha512');
+      const integrity = `sha512-${sha}?one?two?three`;
+      expect(ssri.parse(integrity)).to.deep.equal({
+        source: integrity,
+        digest: sha,
+        algorithm: 'sha512',
+        options: ['one', 'two', 'three'],
+      });
+    });
+
+    it('should omit unsupported algos', function () {
+      const xxx = new Array(50).join('x');
+
+      expect(ssri.parse(`foo-${xxx}`)).to.deep.equal({
+        source: `foo-${xxx}`,
+        algorithm: '',
+        digest: '',
+        options: [],
+      });
+
+      expect(ssri.parse(`sha512-${xxx}`)).to.deep.equal({
+        source: `sha512-${xxx}`,
+        algorithm: 'sha512',
+        digest: xxx,
+        options: [],
+      });
+    });
+
+    it('should discard invalid format entries', function () {
+      const missingDash = 'thisisbad';
+      const missingAlgorithm = '-deadbeef';
+      const missingDigest = 'sha512-';
+
+      expect(ssri.parse(missingDash).toString()).to.be.empty;
+      expect(ssri.parse(missingAlgorithm).toString()).to.be.empty;
+      expect(ssri.parse(missingDigest).toString()).to.be.empty;
+    });
+
+    it('should trim whitespace from either end', function () {
+      const integrity = `      sha512-${hash(TEST_DATA, 'sha512')}    `;
+      expect(ssri.parse(integrity)).to.deep.equal({
+        source: integrity.trim(),
+        algorithm: 'sha512',
+        digest: hash(TEST_DATA, 'sha512'),
+        options: [],
+      });
+    });
+
+    it('should discard hashes that dont abide by the spec', function () {
+      const valid = `sha512-${hash(TEST_DATA, 'sha512')}`;
+      const badAlgorithm = `sha1-${hash(TEST_DATA, 'sha1')}`;
+      const badBase64 = 'sha512-@#$@%#$';
+      const badOpts = `${valid}?\x01\x02`;
+
+      expect(ssri.parse(badAlgorithm).toString()).to.be.empty;
+      expect(ssri.parse(badBase64).toString()).to.be.empty;
+      expect(ssri.parse(badOpts).toString()).to.be.empty;
+    });
+
+    it('should not allow weird stuff in sri', function () {
+      const badInt = 'mdc2\u0000/../../../hello_what_am_I_doing_here-Juwtg9UFssfrRfwsXu+n/Q==';
+
+      expect(ssri.parse(badInt).toString()).to.be.empty;
+    });
+  });
+
+  describe('#verify', function () {
+    let sri;
+
+    before(function () {
+      sri = ssri.parse(`sha512-${hash(TEST_DATA, 'sha512')}`);
+    });
+
+    it('should verify Buffer data', function () {
+      expect(ssri.verify(TEST_DATA, sri)).to.be.true;
+    });
+
+    it('should verify String data', function () {
+      expect(ssri.verify(TEST_DATA.toString('utf8'), sri)).to.be.true;
+    });
+
+    it('should return false when verification fails', function () {
+      expect(ssri.verify('nope', sri)).to.be.false;
+    });
+
+    it('should return false on an invalid sri hash', function () {
+      expect(ssri.verify('nope', 'sha512-nope')).to.be.false;
+    });
+
+    it('should return false on garbage sri input', function () {
+      expect(ssri.verify('nope', 'garbage')).to.be.false;
+    });
+
+    it('should return false on empty sri input', function () {
+      expect(ssri.verify('nope', '')).to.be.false;
+    });
+  });
+});
